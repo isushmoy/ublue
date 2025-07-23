e47ebefde59d3a30a46e45d5ca93d27e9b7e03cf
Sushmoy <80288978+moysush@users.noreply.github.com>
2025-07-23 10:10:18 +0600

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,6 +14,7 @@ dnf5 install -y tmux
 
 # adding vs code
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf install -y code # or code-insiders
 
 # Use a COPR Example:
