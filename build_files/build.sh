9cffba93e763c4fb67ee7e2840a41c67eb2929d2
Sushmoy <80288978+moysush@users.noreply.github.com>
2025-08-03 19:15:04 +0600

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,9 +18,9 @@ set -ouex pipefail
 # dnf install -y v4l2loopback
   
 # adding vs code
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf install -y code # or code-insiders
+# rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+# dnf install -y code # or code-insiders
 
 # Use a COPR Example:
 #
